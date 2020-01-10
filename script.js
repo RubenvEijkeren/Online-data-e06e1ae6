5576de66d51692d03fa9113b68c28dea6ea33c19
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-01-10 10:40:26 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+var obj = {
+	"Name": "Zara",
+	"Score": 36,
+	"StartDate": "21-01-2016",
+	"Friends": ["Mika", "Josef", "Maria", "Sumail", "Lotte"],
+	"Qualified": true,
+}
+console.log(JSON.parse(obj));
